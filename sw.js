809af3f402ac865cb14da8351146d6077d13b849
Lisitheya <elizavetaprosianykh@gmail.com>
2026-05-29 21:41:34 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,35 @@
 const CACHE_NAME = 'kopilochka-v1';
-const ASSETS = ['/'];
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon.png'
+];
+
 self.addEventListener('install', (event) => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
     self.skipWaiting();
 });
+
 self.addEventListener('activate', (event) => {
-    event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+            );
+        })
+    );
     event.waitUntil(clients.claim());
 });
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
